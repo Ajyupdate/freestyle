071e968e05a5e946466b670696df78ede43567c1
Ajibade <ajibadeemmanuel58@gmail.com>
2023-01-06 01:20:35 +0100

    initial commit

---
+++ component/CompanyName.tsx
@@ -0,0 +1,2 @@
+import { Text } from '@chakra-ui/react'
+export const CompanyName = <><Text fontSize='xl'><Text as='span' color='#1CA5AE'>Real</Text ><Text as='span' color='#011213'>Estate</Text ></Text></>
---
+++ modules/landingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../../component/Nav'
+
+export interface iLandingPage {}
+
+const LandingPage = (props: iLandingPage) => {
+  return (
+    <div><Nav/>
+    
+    </div>
+  )
+}
+
+export default LandingPage
