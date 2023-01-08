1b1e9ad30bde5d631d137433c164984a24e3288c
Ajibade <ajibadeemmanuel58@gmail.com>
2023-01-08 19:56:26 +0100

    redesigning the hero

--- component/FirstColumn.tsx
+++ component/FirstColumn.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Flex, Spacer, Heading, Text, Button } from '@chakra-ui/react'
+import { Box, Flex, Spacer, Heading, Text, Button, Center } from '@chakra-ui/react'
 //import { Image } from '@chakra-ui/react'
 import firstColumn from 'public/firstColumn.svg'
 import Image from 'next/image'
@@ -10,17 +10,14 @@ const FirstColumn = (props: iFirstColumn) => {
   return (
     <Box px={16} mt={16}>
         <Flex>
-          <Box  maxW='32rem'>
+          <Box  maxW='45rem'>
             <Heading >Find real estate that suits you. Buy, sell or rent your properties easily.</Heading>
             <Text fontSize='l' mt={8}>
             
               A great place to buy, sell and rent your properties. RealEstatery is here to find you an apartment you want, in the region you want and style you want without any commissions.
             </Text>
 
-            <Box mt={8}>
-              <Image alt='home' src={smallHome}/>
-            </Box>
-
+            
             <Flex gap='4' mt={12} mb={8}>
               <Button size='lg' colorScheme='green' >
                 Browse Properties
@@ -31,38 +28,8 @@ const FirstColumn = (props: iFirstColumn) => {
               </Button>
             </Flex>
 
-            <Box as='hr' color='#03373A'></Box>
+            
 
-            <Box>
-              <Flex gap={4}>
-                <Box>
-                  <Heading as='h5' size={'md'}>
-                    19K+
-                  </Heading>
-                  <Text fontSize='l'>
-                    Properties
-                  </Text>
-                </Box>
-
-                <Box>
-                  <Heading as='h5' size={'md'}>
-                    40m
-                  </Heading>
-                  <Text fontSize='l'>
-                    Market Value
-                  </Text>
-                </Box>
-
-                <Box>
-                  <Heading as='h5' size={'md'}>
-                    1K+
-                  </Heading>
-                  <Text fontSize='l'>
-                    Available Properties
-                  </Text>
-                </Box>
-              </Flex>
-            </Box>
             
           </Box>
 
@@ -70,10 +37,58 @@ const FirstColumn = (props: iFirstColumn) => {
 
           <Box>
             
-            <Image alt='home' src={firstColumn}/>
+            <Center>
+              <Image alt='home' src={smallHome}/>
+            </Center>
+           
             
           </Box>
         </Flex>
+
+        <Box as='hr' color='#03373A'></Box>
+
+        <Center>
+        <Flex gap={4} mt={8}>
+          
+          <Box>
+            <Heading as='h5' size={'md'}>
+              <Center>
+              19K+
+              </Center>
+             
+            </Heading>
+            <Text fontSize='l'>
+              Properties
+            </Text>
+          </Box>
+
+          <Box>
+            <Heading as='h5' size={'md'}>
+              <Center>
+                40m
+              </Center>  
+            </Heading>
+            <Text fontSize='l'>
+              Market Value
+            </Text>
+          </Box>
+
+          <Box>
+
+            
+          <Heading as='h5' size={'md'}>
+          <Center>
+            1K+
+            </Center>
+          </Heading>
+            
+          
+          <Text fontSize='l'>
+            Available Properties
+          </Text>
+          </Box>
+        </Flex>
+        </Center>
     </Box>
   )
 }
