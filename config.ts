41607791b46124ff498c9a386570701012789f9d
Ajibade <ajibadeemmanuel58@gmail.com>
2023-02-06 16:58:40 +0100

    adding font

---
+++ config.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'lora', sans-serif`,
+    body: `'rubik', sans-serif`,
+  },
+})
+
+export default theme
