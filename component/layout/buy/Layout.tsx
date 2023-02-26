2bd7fade0dd98b48dafe1f6e8a42e18196e3db05
Ajibade <ajibadeemmanuel58@gmail.com>
2023-02-26 09:44:09 +0100

    buy layout menu

--- component/layout/buy/Layout.tsx
+++ component/layout/buy/Layout.tsx
@@ -89,7 +89,7 @@ export interface IBuyLayout{
 
 
 
-import { Box, Flex, Heading, SimpleGrid, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Flex, Heading, SimpleGrid, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text, useBreakpointValue } from "@chakra-ui/react";
 import LayoutMenu from "./LayoutMenu";
 import Nav from "./Nav";
 
@@ -106,13 +106,21 @@ function BuyLayout({children, sidebarContent}: IBuyLayout) {
       
 
       {/* Body */}
-      <Flex p="4" flexDirection={{ base: "column", md: "row" }}>
+      {/* <Text>{'298 result'}</Text> */}
+      <Flex p="4" px={{base: 6, md:16}} flexDirection={{ base: "column", md: "row" }}>
         {/* Main Component */}
-        <Box w={mainWidth} p="4" bg="white" boxShadow="md">
-          <Heading size="lg" mb="4">
-            Main Component
+        <Box w={mainWidth} p="4" bg="white">
+
+          <Heading size="md" mb="4">
+            Popular Residencies
           </Heading>
-          {children}
+          
+          <Box h="500px" overflowY="scroll">
+            {children}
+          </Box>
+            
+         
+          
           {/* <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}> */}
             {/* Column 1 */}
             {/* <Box bg="gray.100" p="4" boxShadow="md"> */}
